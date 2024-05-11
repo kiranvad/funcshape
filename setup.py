5d7397f81617377db567ace440e02a73f6ecae4b
Kiran Vaddi <pozzolabadmin@Chemicals-MacBook-Pro-2.local>
2024-05-10 18:20:12 -0700

    add functionality to align functions--works as good as DP in most cases but sometimes gets trapped locally

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
-from setuptools import setup
+from setuptools import setup,find_packages
+import sys, os
 
-setup(
-    name='funcshape',
-    version='1.0',
-    packages=["funcshape"],
-    description="Implementation of the deep reparametrization algorithm in the accompanying paper.",
-    author="Jørgen Riseth",
-    author_email="jnriseth@gmail.com"
-)
+def parse_requirements_file(filename):
+    """Read the lines of the requirements file."""
+    with open(filename) as input_file:
+        return input_file.read().splitlines()
+    
+if __name__ == '__main__':
+    requirements = parse_requirements_file('requirements.txt')
+    install_requires = []
+    optional_dependencies = {}
+    
+    for requirement in requirements:
+            install_requires.append(requirement)  
+    setup(name="funcshape",
+          description="Obtain warping functions using deep learning based reprametrization",
+          version='1.0',
+          author='Kiran Vaddi',
+          author_email='kiranvad@uw.edu',
+          license='MIT',
+          python_requires='>=3.8',
+          install_requires=install_requires,
+          extras_require = {},
+          packages=find_packages(),
+          long_description=open('README.md').read(),
+          long_description_content_type="text/markdown",
+          classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Operating System :: Microsoft :: Windows"
+          ],
+    )
